9b0934da02671cb09478e297959cc9887f8b5f51
Meade Reamy <meade@reamy.com>
2023-05-30 13:58:44 -0400

    app.js done

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 // TODO
+const { useState } = React;
+const ListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+  const style = {
+    fontWeight: isDone ? 'bold' : 'normal',
+  };
+ return (
+   <li style={style} onMouseOver={() => setIsDone(!isDone)}>
+   {props.item}
+   </li>
+ );
+};
+
+const Grocery = (props) => (
+<ol>
+{props.item.map((item) => (
+  <ListItem item={item} />
+))}
+</ol>
+);
+
+const App = () =>  (
+  <div>
+    <h2>Grocery List</h2>
+    <Grocery item={['apples', 'peanuts']} />
+  </div>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById(
+    'app'
+  )
+);
